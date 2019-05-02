80b1d3d5dd56070f652df87e799fb2d152ef368d
3873896 <3873896@ppti-14-507-02.ufr-info-p6.jussieu.fr>
2019-05-02 17:42:40 +0200

    plop

--- LodeRunner/src/services/ItemService.java
+++ LodeRunner/src/services/ItemService.java
@@ -2,8 +2,6 @@ package services;
 
 public interface ItemService {
 	
-	public void init(int id, ItemType nature, int x, int y);
-	
 	public int getId();
 	
 	public ItemType getNature();
@@ -12,7 +10,26 @@ public interface ItemService {
 	
 	public int getColumn();
 	
+	/**
+	 * pre : 0 <= id 
+	 * pre : 0 <= x 
+	 * pre : 0 <= y
+	 * post : getId() == id
+	 * post : getNature() == nature
+	 * post : getColumn() == x
+	 * post : getHeight() == y
+	 */
+	public void init(int id, ItemType nature, int x, int y);
+	
+	/**
+	 * pre : 0 <= x
+	 * post : getColumn() == x
+	 */
 	public void setColumn(int x);
 	
+	/**
+	 * pre : 0 <= y
+	 * post : getHeight() == y
+	 */
 	public void setHeight(int y);
 }
--- LodeRunner/src/services/PlayerService.java
+++ LodeRunner/src/services/PlayerService.java
@@ -3,6 +3,14 @@ package services;
 public interface PlayerService extends CharacterService{
 	
 	public EngineService getEngine();
+	
+	/**
+	 * post : getEngine() === e
+	 */
 	public void setEngine(EngineService e);
+	
+	/**
+	 * 
+	 */
 	public void step();
 }
